68faf793326f02432f25c52ae7af3a0b88484518
hugo <hugof16silva@gmail.com>
2023-11-02 05:48:21 -0300

    colocando mais condição de mensagem

--- sistemaCorrecaoProvas.js
+++ sistemaCorrecaoProvas.js
@@ -12,7 +12,7 @@ const prova = {
             correta: "c"
         },
         {
-            resposta: "e",
+            resposta: "b",
             correta: "b"
         },
         {
@@ -20,21 +20,35 @@ const prova = {
             correta: "b"
         },
         {
-            resposta: "c",
+            resposta: "a",
             correta: "c"
         }
     ]
 };
 
 function corrigirProva(prova) {
-    let resultado = 0
+    let resultadoAcertado = 0
     for (const acertou of prova.questoes) {
-        acertou.resposta === acertou.correta ? resultado++ : ''
+        acertou.resposta === acertou.correta ? resultadoAcertado++ : ''
     }
-    let total = resultado * 2
-    console.log(`
-    O aluno(a) ${prova.aluno} acertou ${resultado} questões e obteve nota ${total}
-    `);
+    let valorQuestao = prova.valor / prova.questoes.length
+    let totalAcertado = resultadoAcertado * valorQuestao
+
+    resultadoAcertado < 1 ?
+        console.log(`
+    O aluno(a) ${prova.aluno} acertou ${resultadoAcertado} de ${prova.questoes.length} questão e obteve nota ${totalAcertado} TEM QUE ESTUDAR MAIS !!
+    `) :
+        resultadoAcertado === 1 ?
+            console.log(`
+    O aluno(a) ${prova.aluno} acertou ${resultadoAcertado} de ${prova.questoes.length} questão e obteve nota ${totalAcertado} PRECISA SE ESFORÇA !!
+    `) :
+            resultadoAcertado === prova.questoes.length ?
+                console.log(`
+    O aluno(a) ${prova.aluno} acertou ${resultadoAcertado} de ${prova.questoes.length} questões e obteve nota ${totalAcertado} PARABÉNS !!
+    `) :
+                console.log(`
+    O aluno(a) ${prova.aluno} acertou ${resultadoAcertado} de ${prova.questoes.length} questões e obteve nota ${totalAcertado} VOCÊ CONSEGUE !!
+    `)
 }
 
 corrigirProva(prova)
